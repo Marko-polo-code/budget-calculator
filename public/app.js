9ec7364f50559677d4a36fed031e331ef314e0af
Mark Muller <mark.muller.w@gmail.com>
2023-10-24 12:14:09 +0200

    adding the rendering of the list of incomes and expenses

--- public/app.js
+++ public/app.js
@@ -1,29 +1,13 @@
 import { Expense } from './classes/expense.js';
 import { Income } from './classes/income.js';
-// let docOne: hasFormatter;
-// let docTwo: hasFormatter;
-// docOne = new Expense('o2', 'internet and phone bill', 50);
-// docTwo = new Income('coding', 'work', 2700);
-// let docs: hasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const expenseOne = new Expense('The mortgage', 'the flat', 1250);
-// const expensetwo = new Expense('Account', 'savings', 600);
-// //console.log(expenseOne);
-// let expenses: Expense[] = [];
-// expenses.push(expenseOne);
-// expenses.push(expensetwo);
-// console.log(expenses);
-// expenses.forEach(exp => {
-//   console.log(exp.account, exp.details, exp.amount, exp.format());
-// });
+import { listTemplate } from './classes/listTemplate.js';
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new listTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -33,5 +17,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Expense(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,17 @@
 import { Expense } from './classes/expense.js';
 import { Income } from './classes/income.js';
+import { listTemplate } from './classes/listTemplate.js';
 import { hasFormatter } from './interfaces/hasFormatter.js';
 
-// let docOne: hasFormatter;
-// let docTwo: hasFormatter;
-
-// docOne = new Expense('o2', 'internet and phone bill', 50);
-// docTwo = new Income('coding', 'work', 2700);
-
-// let docs: hasFormatter[] = [];
-
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const expenseOne = new Expense('The mortgage', 'the flat', 1250);
-// const expensetwo = new Expense('Account', 'savings', 600);
-
-// //console.log(expenseOne);
-// let expenses: Expense[] = [];
-
-// expenses.push(expenseOne);
-// expenses.push(expensetwo);
-
-// console.log(expenses);
-// expenses.forEach(exp => {
-//   console.log(exp.account, exp.details, exp.amount, exp.format());
-// });
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new listTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
@@ -48,5 +24,5 @@ form.addEventListener('submit', (e: Event) => {
     doc = new Expense(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, 'end');
 });
